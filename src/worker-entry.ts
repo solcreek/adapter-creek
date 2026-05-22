4500c97d63a2e34d5c32268eacaf79c1b3c466c6
Lawrence Lin <deduce@gmail.com>
2026-05-22 01:06:36 -0500

    fix: preserve cached fetch init shape

--- src/worker-entry.ts
+++ src/worker-entry.ts
@@ -3175,9 +3175,6 @@ function __creekOriginalFetchArgs(input, init) {
     }
     delete fetchInit.next;
     delete fetchInit.cache;
-    try {
-      return [new Request(input, fetchInit), undefined];
-    } catch {}
     return [input, fetchInit];
   }
   return [input, init];
